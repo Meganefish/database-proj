8a8ed0c338d1ecfbcb74ebe950fd54b59ee51d62
meganefish <1519624106@qq.com>
2024-10-27 20:32:58 +0800

    commision 01

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,51 +1,49 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request,url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required 
+from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('blog',__name__) # 无urlprefix，因此用于根目录
+bp = Blueprint('blog', __name__)  # 无urlprefix，因此用于根目录
+
 
 @bp.route('/')
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body,created, author_id, username, forum_name'
-        ' FROM post p JOIN user u ON p.author_id = u.id JOIN forums f ON p.forum_id = f.forum_id'
-        ' ORDER BY created DESC'
+        'SELECT p.id, title, body,created, author_id, username, forum_name, p.forum_id'
+        ' FROM post p '
+        'JOIN user u ON p.author_id = u.id '
+        'JOIN forums f ON p.forum_id = f.forum_id'
+        ' ORDER BY created DESC, p.id DESC'
     ).fetchall()
-    return render_template('blog/index.html',posts=posts)
+    return render_template('blog/index.html', posts=posts)
+
 
 # 创建视图
-@bp.route('/create',methods=('GET','POST'))
+@bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
+    db = get_db()
+    forums = db.execute('SELECT forum_id, forum_name FROM forums').fetchall()
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-
-        error = None
+        forum_id = request.form.get('forum_id')  # 获取选择的论坛 ID
 
         if not title:
-            error = 'Title is required.'
-
-        db = get_db()
-        forums = db.execute(
-            'SELECT forum_id, forum_name FROM forums'
-        ).fetchall()
-        if error is not None:
-            flash(error)
+            flash('Title is required.')
         else:
             db.execute(
-                'INSERT INTO post (title,body,author_id)'
-                'VALUES (?,?,?)',
-                (title,body,g.user['id'])
+                'INSERT INTO post (title,body,author_id,forum_id) VALUES (?,?,?,?)',
+                (title, body, g.user['id'], forum_id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
-    return render_template('blog/create.html')
+    return render_template('blog/create.html', forums=forums)
+
 
 def get_post(id, check_author=True):
     post = get_db().execute(
@@ -53,17 +51,18 @@ def get_post(id, check_author=True):
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
-    ).fetchone() # id
+    ).fetchone()  # id
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
-    
+
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
-    
+
     return post
 
-@bp.route('/<int:id>/update',methods=('GET','POST'))
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
     post = get_post(id)
@@ -87,14 +86,35 @@ def update(id):
             )
             db.commit()
             return redirect(url_for('blog.index'))
-        
-    return render_template('blog/update.html',post=post)
-    
-@bp.route('/<int:id>/delete',methods=('POST',))
+
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?',(id,))
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+@bp.route('/f/<int:forum_id>', methods=('GET', 'POST'))
+def forum(forum_id):
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body,created, author_id, username, forum_name'
+        ' FROM post p '
+        'JOIN user u ON p.author_id = u.id '
+        'JOIN forums f ON p.forum_id = f.forum_id'
+        ' WHERE f.forum_id = ?'
+        ' ORDER BY created DESC, p.id DESC',
+        (forum_id,)
+    ).fetchall()
+    forum_name_row = db.execute(
+        'SELECT forum_name FROM forums WHERE forum_id = ?',
+        (forum_id,)
+    ).fetchone()
+    forum_name = forum_name_row['forum_name'] if forum_name_row else "forum not found"
+    return render_template('blog/forum.html', posts=posts, forum_name=forum_name)
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS forums;
 DROP TABLE IF EXISTS moderators;
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
     nickname VARCHAR(50)
 );
 CREATE TABLE post (
@@ -32,8 +32,22 @@ CREATE TABLE moderators (
     FOREIGN KEY (forum_id) REFERENCES forums (forum_id)
 );
 
+
 INSERT INTO forums(forum_name, description)
 VALUES ('学业', '有关学习'),
        ('选课','有关选课'),
        ('娱乐', '有关娱乐'),
        ('树洞', '有关树洞');
+INSERT INTO user(username, password, nickname)
+values ('123', '123', '123'),
+       ('abc', 'abc', 'abc'),
+       ('feibo','123', 'feibo'),
+       ('damdam','123','damdam'),
+       ('tutu', '123', 'tutu');
+INSERT INTO post(author_id, forum_id, title, body)
+values ('1', '1', 'abc', 'abcdefg'),
+       ('1', '2', '123', '1234567'),
+       ('2', '3', 'asdsdasd', 'asdasdfasdfasdfas'),
+       ('3','4','asdafadf','chaoshidamdam'),
+       ('4','4','asdafadfadf','chaoshitutu'),
+       ('5','4','sadafsdf','chaoshifeibo');
